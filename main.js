4e699c7fa929fa22fad7745ba175745adb3ebbb0
Mayrus Michel <101563384+mayrusmichel@users.noreply.github.com>
2022-05-06 09:52:00 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+const floatExpand = document.getElementById('float');
+
+
+//NAVBAR SCROLL LOCK-add_remove Class onScroll
+function onScroll() {
+    if(scrollY > 0) {
+        navigation.classList.add('header-scroll')
+    } else {
+        navigation.classList.remove('header-scroll')
+    }
+};
+
+
+//FLOAT BUTTON EXPAND-add_remove Class onClick
+function floatExpanded() {
+    if (float.classList.contains('float-expand')) {
+        float.classList.remove('float-expand')
+    } else {
+        float.classList.add('float-expand')
+    }
+}
+
